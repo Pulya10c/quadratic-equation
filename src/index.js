a4a5f92d8c07db9da50143db66fcdac7a83a6b60
Pulya10c <sergienya.oleg@yandex.ru>
2018-09-18 22:13:42 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ module.exports = function solveEquation(equation) {
   // your implementation
 var TestStr = equation;
 
-equation.substring
 if (TestStr[0] == "-") {
   var A =  - TestStr.substring(1, TestStr.indexOf(" * x^2 "));
 } 
@@ -11,17 +10,20 @@ else {var A = + TestStr.substring(0, TestStr.indexOf(" * x^2 "));}
 if (TestStr.substr(TestStr.indexOf(" * x^2 ")+7, 1) == "-") {
   var B =  - TestStr.substring(TestStr.indexOf("x^2 ")+5, TestStr.indexOf(" * x "));
 } 
-else {var B = + TestStr.substring(TestStr.indexOf("x^2 ")+7, TestStr.indexOf(" * x ")); }
+else {var B = + TestStr.substring(TestStr.indexOf("x^2 ")+5, TestStr.indexOf(" * x ")); }
 
 if (TestStr.substr(TestStr.indexOf("x ")+2, 1) == "-") {
   var C =  - TestStr.substring(TestStr.indexOf("x ")+4);
 } 
-else {var C = + TestStr.substring(TestStr.indexOf("x ")+5);}
+else {var C = + TestStr.substring(TestStr.indexOf("x ")+4);}
 
+A = Number(A);
+B = Number(B);
+C = Number(C);
 
-var D=B*B-4*A*C;
-var X=Math.trunc(( - B + Math.sqrt(D)) / 2);
-var X1=Math.trunc(( - B - Math.sqrt(D)) / 2);
+var X = Math.round((- B - Math.sqrt(B*B-4*A*C))/(2*A));
+var X1 = Math.round((- B + Math.sqrt(B*B-4*A*C))/(2*A));
+
 var MyAns = [];
   if (X>X1) {
   MyAns[0] = X1;
